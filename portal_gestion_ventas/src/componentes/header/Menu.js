d45e5a1d7de6d421db2716621bfa5fdaa9a08610
LEYDY VALENCIA <LEIDY.VALENCIA77@GMAIL.COM>
2021-10-12 21:36:25 -0500

    Migración a ReactJS

---
+++ portal_gestion_ventas/src/componentes/header/Menu.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './Menu.css';
+import logo from './imagen1.png';
+
+class Menu extends React.Component{
+    render(){
+        return (
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+			<img src={logo} className="menu-img" alt="Responsive image" />
+			<a className="navbar-brand" href="#">Gestión de ventas</a>
+		    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+		     	<span className="navbar-toggler-icon"></span>
+		    </button>
+		    <div className="collapse navbar-collapse menu-submenu" id="navbarCollapse">
+			    <ul className="navbar-nav mr-auto nav">
+			        <li className="nav-item active">
+			          	<a className="nav-link" href="#">Gestionar ventas</a>
+						<ul>
+							<li>
+								<a href="../html/registrar_venta.html">
+									Registrar venta
+								</a>
+							</li>
+							<li>
+								<a href="../html/consultar_ventas.html">
+									Consultar ventas
+								</a>
+							</li>
+                    	</ul>
+			        </li>
+			        <li className="nav-item">
+			          	<a className="nav-link" href="#">Gestionar productos</a>
+						<ul>
+							<li>
+								<a href="../html/registrar_producto.html">Registrar producto</a>
+							</li>
+							<li>
+								<a href="../html/consultar_productos.html">
+									Consultar productos
+								</a>
+							</li>
+                    	</ul>
+			        </li>
+			        <li className="nav-item">
+			          	<a className="nav-link" href="#">Gestionar usuarios</a>
+						<ul>
+							<li>
+								<a href="../html/lista_usuarios.html">Mostrar usuarios</a>
+							</li>
+							<li>
+								<a href="../html/crud_usuarios.html">Administrar usuarios</a>
+							</li>
+                    	</ul>
+			        </li>
+			        <li className="nav-item">
+			          	<a className="nav-link" href="#">Contacto</a>
+			        </li>
+			    </ul>
+		    </div>
+			<div className="collapse navbar-collapse" id="navbarCollapse">
+				<form className="form-inline mt-2 mt-md-0">
+			        <button className="menu-button my-sm-0" type="submit">Cerrar sesión</button>
+			    </form>
+			</div>
+		</nav>
+        )
+    }
+}
+export default Menu;
