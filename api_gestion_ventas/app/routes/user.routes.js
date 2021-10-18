77a0f1dbbabc018bca50af674e67a81b5ee24217
LEYDY VALENCIA <LEIDY.VALENCIA77@GMAIL.COM>
2021-10-18 10:24:12 -0500

    Ajuste de Esquema de Colecciones para Usuarios, Productos y Ventas

--- api_gestion_ventas/app/routes/user.routes.js
+++ api_gestion_ventas/app/routes/user.routes.js
@@ -1,30 +1,38 @@
-const { users } = require("../models/index.js");
+const {
+  users
+} = require("../models/index.js");
 
 module.exports = app => {
-    const users = require("../controllers/user.controller.js");
-  
-    var router = require("express").Router();
-  
-    // Create a new User
-    router.post("/", users.create);
-  
-    // Retrieve all Users
-    router.get("/", users.findAll);
-  
-    // Retrieve all published Users
-    router.get("/published", users.findAllPublished);
-  
-    // Retrieve a single User with id
-    router.get("/:id", users.findOne);
-  
-    // Update a User with id
-    router.put("/:id", users.update);
-  
-    // Delete a User with id
-    router.delete("/:id", users.delete);
-  
-    // Delete all Users
-    router.delete("/", users.deleteAll);
-  
-    app.use('/api/users', router);
-  };
+  const users = require("../controllers/user.controller.js");
+
+  var router = require("express").Router();
+
+  // Create a new User
+  router.post("/create", users.create);
+
+  // Retrieve all Users
+  router.get("/retrieve", users.findAll);
+
+  // Retrieve all the users who are Clients
+  router.get("/clients", users.findAllClients);
+
+  // Retrieve all the users who are Sellers
+  router.get("/sellers", users.findAllSellers);
+
+  // Retrieve a single User with id
+  router.get("/retrieve/:id", users.findOne);
+
+  // Update a User with id
+  router.put("/update/:id", users.update);
+
+  // Find Clients which name contains an text
+  router.get("/find/:name", users.findForContains);
+
+  // Find Clients which name contains an text
+  router.get("/verify/:token", users.verify);
+
+  // Delete all Users
+  router.delete("/delete", users.deleteAll);
+
+  app.use('/api/users', router);
+};
