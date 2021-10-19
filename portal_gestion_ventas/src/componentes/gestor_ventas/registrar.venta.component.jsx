317f6788b12c9140f6269517f930789368312fdf
William <leonardo.prado.cespedes@hotmail.com>
2021-10-18 21:19:32 -0500

    Formulario de ventas

--- portal_gestion_ventas/src/componentes/gestor_ventas/registrar.venta.component.jsx
+++ portal_gestion_ventas/src/componentes/gestor_ventas/registrar.venta.component.jsx
@@ -1,183 +1,157 @@
 import React from 'react';
-import saleDataService from '../../services/gestor_venta/sale.service';
-import { Form, Button, Row, Col} from 'react-bootstrap'
+import SaleDataService from '../../services/gestor_ventas/sale.service';
+import { Form, Button, Row, Col } from 'react-bootstrap'
 
 
-class RegistrarVenta extends React.Component{
+class RegistrarVenta extends React.Component {
 
-    constructor(props, context) {
-		super(props, context);
-		
-		this.handleClick = this.handleClick.bind(this);
+  constructor(props, context) {
+    super(props, context);
+
     this.state = {
       sale: {
-          cliente: "",
-          valor: "",
-          cantidad: "",
-          vendedor: "",
-          fecha: ""
+        status: {
+          name: "",
+          value: "",
+        },
+        registeredBy: {
+          nameEmployee: "",
+          idEmployee: "",
+        },
+        boughtBy: {
+          nameClient: "",
+          idClient: ""
+        },
+        productsBox: [],
+        total: 0
       }
-  	}
+    }
     this.handleFormChange = this.handleFormChange.bind(this);
   };
 
   handleFormChange = event => {
     debugger;
     let saleNew = {
-        ...this.state.sale
+      ...this.state.sale
     };
     let val = event.target.value;
-    if (event.target.name === "role") {
-        saleNew[event.target.name] = {name: val, value : val};
-    }else{
-        saleNew[event.target.name] = val;
+    if (event.target.name === "status") {
+      saleNew[event.target.name] = { name: val, value: val };
+    } else {
+      saleNew[event.target.name] = val;
     }
     this.setState({
-        sale: userNew
+      sale: saleNew
     });
-};
+  };
 
-createSale = event => {
+  createSale = event => {
+    debugger;
     let sale = this.state.sale;
-    saleDataService.create(sale)
-        .then(response => {
-            this.setState({
-                id_venta: response.sale.id,
-                status: response.sale.status,
-                description: response.sale.description,
-                role: response.sale.role,
-                email: response.sale.email,
-                password: response.sale.password,
+    SaleDataService.create(sale)
+      .then(response => {
+        this.setState({
+          status: response.sale.status,
+          registeredBy: response.sale.registeredBy,
+          boughtBy: response.sale.boughtBy,
+          productsBox: [],
+          total: response.sale.total,
 
-                submitted: true
-            });
-            console.log(response.sale);
-        })
-        .catch(e => {
-            console.log(e);
-        });debugger;
-        //event.preventDefault();
-};
-newSale() {
+          submitted: true
+        });
+        console.log(response.sale);
+      })
+      .catch(e => {
+        console.log(e);
+      }); debugger;
+    //event.preventDefault();
+  };
+  newSale() {
     this.setState({
+      status: {
         name: "",
-        status: {
-            name: "",
-            value: ""
-        },
-        description: "",
-        role: {
-            name: "",
-            value: ""
-        },
-        email: "",
-        password: "",
-
-        submitted: false
+        value: "",
+      },
+      registeredBy: {
+        nameEmployee: "",
+        idEmployee: "",
+      },
+      boughtBy: {
+        nameClient: "",
+        idClient: ""
+      },
+      productsBox: [],
+      total: 0
     });
-}
+  }
 
-    render(){
-        return (
+  render() {
+    return (
 
-            <>
-            <section>
-               <div>
-                   <h3>
-                       Ingrese los datos de la venta
-                   </h3>
-               </div>
-            </section>
-            <Form>
-  <Row className="mb-3">
-    <Form.Group as={Col} controlId="formGridID">
-      <Form.Label>ID del producto</Form.Label>
-      <Form.Control 
-       id="selectedItem"
-       name="selectedItem"
-       type="text"
-       className="form-control"
-       placeholder="Id del producto"
-       onChange={this.handleFormChange} />
-
-
-    </Form.Group>
-
-    <Form.Group as={Col} controlId="formGridDescripcion">
-      <Form.Label>ID cliente</Form.Label>
-      <Form.Control 
-      type="text"
-       placeholder="Id cliente" 
-       id="boughtBy"
-       name="boughtBy"
-       type="text"
-       />
-    </Form.Group>
-  </Row>
+      <>
+        <section>
+          <div>
+            <h3>
+              Ingrese los datos de la venta
+            </h3>
+          </div>
+        </section>
+        <Form onSubmit={this.createSale}>
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridID">
+              <Form.Label>ID del producto</Form.Label>
+              <Form.Control
+                id="productsBox"
+                name="productsBox"
+                type="text"
+                className="form-control"
+                placeholder="Id del producto"
+                onChange={this.handleFormChange} />
 
 
-  <Row className="mb-3">
-    <Form.Group as={Col} controlId="formGridNumber">
-      <Form.Label>Valor Unitario</Form.Label>
-      <Form.Control 
-      type="number"
-      placeholder="valor" 
-      id="boughtBy"
-      name="boughtBy"
-      type="text"
-      />
-      
-    </Form.Group>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridDescripcion">
+              <Form.Label>ID cliente</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Id cliente"
+                id="boughtBy"
+                name="boughtBy"
+                type="text"
+              />
+            </Form.Group>
+          </Row>
 
 
-    <Form.Group as={Col} controlId="formGridCantidad">
-      <Form.Label>Cantidad</Form.Label>
-      <Form.Control
-      type="number"
-      placeholder="Cantidad" 
-      id="boughtBy"
-      name="boughtBy"
-      type="text"
-      />
-    </Form.Group>
-  </Row>
+          <Row className="mb-3">
 
-  <Row className="mb-3">
-    <Form.Group as={Col} controlId="formGridIDvendedor">
-      <Form.Label>ID vendedor</Form.Label>
-      <Form.Control
-      type="number"
-      placeholder="Id vendedor" 
-      id="boughtBy"
-      name="boughtBy"
-      type="text"
-      />
-    </Form.Group>
 
-   
 
-    <Form.Group as={Col} controlId="formGridFecha">
-      <Form.Label>Fecha</Form.Label>
-      <Form.Control />
-    </Form.Group>
-  </Row>
+            <Form.Group as={Col} controlId="formGridCantidad">
+              <Form.Label>Cantidad</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Cantidad"
+                id="amount"
+                name="amount"
+                type="text"
+              />
+            </Form.Group>
+          </Row>
 
-  
+          <div class="text-center">
 
-  
+            <Button type="submit" value="RegistrarVenta" variant="outline-danger">Registrar</Button>
 
- <div class="text-center">
+          </div>
 
-  <Button href="/search_sale" variant="outline-danger">Registrar</Button> 
-  
-</div> 
+        </Form>
 
-</Form>
-            
-            
-            
-            </>
-        )
-    }
+
+
+      </>
+    )
+  }
 }
 export default RegistrarVenta;
