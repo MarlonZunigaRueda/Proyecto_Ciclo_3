75894984cbf4770eb35731ab984cc776aed3e53a
LEYDY VALENCIA <LEIDY.VALENCIA77@GMAIL.COM>
2021-10-18 20:07:06 -0500

    Ajuste de Esquema de Coleccion de  Productos

--- api_gestion_ventas/app/routes/product.routes.js
+++ api_gestion_ventas/app/routes/product.routes.js
@@ -1,30 +1,26 @@
-const { products } = require("../models/index.js");
+const {
+  products
+} = require("../models/index.js");
 
 module.exports = app => {
-    const products = require("../controllers/product.controller.js");
-  
-    var router = require("express").Router();
-  
-    // Create a new Product
-    router.post("/", products.create);
-  
-    // Retrieve all Products
-    router.get("/", products.findAll);
-  
-    // Retrieve all published Products
-    router.get("/published", products.findAllPublished);
-  
-    // Retrieve a single Product with id
-    router.get("/:id", products.findOne);
-  
-    // Update a Product with id
-    router.put("/:id", products.update);
-  
-    // Delete a Product with id
-    router.delete("/:id", products.delete);
-  
-    // Delete all Products
-    router.delete("/", products.deleteAll);
-  
-    app.use('/api/products', router);
-  };
+  const products = require("../controllers/product.controller.js");
+
+  var router = require("express").Router();
+
+  // Create a new Product
+  router.post("/create", products.create);
+
+  // Retrieve all Products
+  router.get("/retrieve", products.findAll);
+
+  // Retrieve a single Product with id
+  router.get("/retrieve/:id", products.findOne);
+
+  // Update a Product with id
+  router.put("/update/:id", products.update);
+
+  // Delete all Products
+  router.delete("/delete", products.deleteAll);
+
+  app.use('/api/products', router);
+};
