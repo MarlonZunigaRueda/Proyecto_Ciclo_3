d8c21e0c4d84bd6bb14e184d0cfd036f699103da
Mateo Soto Arango <mateo.sotoa@udea.edu.co>
2021-10-18 20:12:33 -0500

    Servicios productos

--- portal_gestion_ventas/src/componentes/gestor_productos/registrar.producto.component.jsx
+++ portal_gestion_ventas/src/componentes/gestor_productos/registrar.producto.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProductsDataService from '../../services/gestor_productos/product.services'
 import { Form, Button, Row, Col} from 'react-bootstrap'
 
 
@@ -6,16 +7,81 @@ class RegistrarProducto extends React.Component{
 
     constructor(props, context) {
 		super(props, context);
-		
-		this.handleClick = this.handleClick.bind(this);
-  	}
 
-	handleClick(){
-        debugger;
-		console.log("algo");
-		//this.props
-		//console.log(this.refs.name);
-  	}
+    this.state = {
+      product: {
+        description: String,
+        status: {
+          name: String,
+          value: String
+        },
+        amount: Number,
+        unitValue: Number,
+        registeredBy: {
+          nameEmployee: String,
+          idEmployee: String
+        }
+      }
+  };
+		
+	 this.handleFormChange = this.handleFormChange.bind(this);
+
+  }
+
+    handleFormChange = event => {
+      debugger;
+      let productNew = {
+          ...this.state.product
+      };
+      let val = event.target.value;
+      if (event.target.name === "role") {
+          productNew[event.target.name] = {name: val, value : val};
+      }else{
+          productNew[event.target.name] = val;
+      }
+      this.setState({
+          product: productNew
+      });
+  };
+
+  createproduct = event => {
+      let products = this.state.products;
+      ProductsDataService.create(products)
+          .then(response => {
+              this.setState({
+                  name: response.products.name,
+                  status: response.products.status,
+                  description: response.product.description,
+                  role: response.product.role,
+                  email: response.product.email,
+                  password: response.product.password,
+
+                  submitted: true
+              });
+              console.log(response.product);
+          })
+          .catch(e => {
+              console.log(e);
+          });debugger;
+          //event.preventDefault();
+  };
+  newProduct() {
+      this.setState({
+        description: String,
+        status: {
+          name: String,
+          value: String
+        },
+        amount: Number,
+        unitValue: Number,
+        registeredBy: {
+          nameEmployee: String,
+          idEmployee: String
+        },
+
+          submitted: false
+      });
+  }
 
     render(){
         return (
@@ -28,16 +94,29 @@ class RegistrarProducto extends React.Component{
                    </h3>
                </div>
             </section>
-            <Form>
+            <Form onSubmit={this.newProduct}>
   <Row className="mb-3">
     <Form.Group as={Col} controlId="formGridID">
-      <Form.Label>ID del producto</Form.Label>
-      <Form.Control type="password" placeholder="Id producto" />
+      <Form.Label>Estado del producto</Form.Label>
+      <Form.Control 
+       id= "status"
+       name = "status"
+       type="text"
+       className="form-control"
+       placeholder="Estado"
+       onChange={this.handleFormChange}
+        />
     </Form.Group>
 
     <Form.Group as={Col} controlId="formGridDescripcion">
       <Form.Label>Descripción</Form.Label>
-      <Form.Control type="text" placeholder="Descripción" />
+      <Form.Control 
+        id="name"
+        name="name"
+        type="text"
+        className="form-control"
+        onChange={this.handleFormChange}
+        placeholder="Descripción" />
     </Form.Group>
   </Row>
 
@@ -46,14 +125,28 @@ class RegistrarProducto extends React.Component{
   <Row className="mb-3">
     <Form.Group as={Col} controlId="formGridNumber">
       <Form.Label>Valor Unitario</Form.Label>
-      <Form.Control />
+      <Form.Control 
+      id="unitValue"
+      name="unitValue"
+      type="number"
+      className="form-control"
+      onChange={this.handleFormChange}
+      placeholder="Valor" />
+
     </Form.Group>
 
    
 
     <Form.Group as={Col} controlId="formGridCantidad">
       <Form.Label>Cantidad</Form.Label>
-      <Form.Control />
+      <Form.Control
+       id="amount"
+       name="amount"
+       type="number"
+       className="form-control"
+       onChange={this.handleFormChange}
+       placeholder="Cantidad" />
+      
     </Form.Group>
   </Row>
 
@@ -61,7 +154,7 @@ class RegistrarProducto extends React.Component{
 
  <div class="text-center">
 
-  <Button href="/search_product" variant="outline-danger">Registrar</Button> 
+  <Button href="#" variant="outline-danger">Registrar</Button> 
   
 </div> 
 
